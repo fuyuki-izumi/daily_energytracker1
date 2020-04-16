aa495b5bffd7fab75845ce0fc91d8bebe5adee41
fuyuki.izumi <mac1@mac1-no-MacBook-Pro.local>
2020-04-16 09:22:37 +0900

    Add EmployeeClass

---
+++ src/models/Employee.java
@@ -0,0 +1,126 @@
+package models;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+
+@Table(name = "employees")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllEmployees",
+            query = "SELECT e FROM Employee AS e ORDER BY e.id DESC"
+            ),
+    @NamedQuery(
+            name = "getEmployeeCount",
+            query = "SELECT COUNT(e) FROM Employee AS e"
+            ),
+    @NamedQuery(
+            name = "checkRegisteredCode",
+            query = "SELECT COUNT(e) FROM Employee AS e WHERE e.code = :code"
+            ),
+    @NamedQuery(
+            name ="checkLoginCodeAndPassword",
+            query = "SELECT e FROM Employee AS e WHERE e.delete_flag = 0 AND e.code = :code AND e.password = :pass"
+            )
+})
+
+@Entity
+public class Employee {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "code", nullable = false, unique = true)
+    private String code;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "password", length = 64, nullable = false)
+    private String password;
+
+    @Column(name = "admin_flag", nullable = false)
+    private Integer admin_flag;
+
+    @Column(name = "created_at", nullable = false)
+    private Timestamp created_at;
+
+    @Column(name = "updated_at", nullable = false)
+    private Timestamp  updated_at;
+
+    @Column(name = "delete_flag", nullable = false)
+    private Integer delete_flag;
+
+    public Integer getId(){
+        return id;
+    }
+
+    public void setId(Integer id){
+        this.id =id;
+    }
+
+    public String getCode(){
+        return code;
+    }
+
+    public void setCode(String code){
+        this.code = code;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public Integer getAdmin_flag(){
+        return admin_flag;
+    }
+
+    public void setAdmin_flag(Integer admin_flag){
+        this.admin_flag = admin_flag;
+    }
+
+    public Timestamp getCreated_at(){
+        return created_at;
+    }
+
+    public void setCreated_at(Timestamp created_at){
+        this.created_at = created_at;
+    }
+
+    public Timestamp getUpdated_at(){
+        return updated_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at){
+        this.updated_at = updated_at;
+    }
+
+    public Integer getDelete_flag(){
+        return delete_flag;
+    }
+
+    public void setDelete_flag(Integer delete_flag){
+        this.delete_flag = delete_flag;
+    }
+}
---
+++ src/validators/EmployeeValidator.java
@@ -0,0 +1,72 @@
+package validators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import models.Employee;
+import utils.DBUtil;
+
+public class EmployeeValidator {
+    public static List<String> validate(Employee e, Boolean code_duplicate_check_flag, Boolean password_check_flag){
+        List<String> errors = new ArrayList<String>();
+
+        String code_error = _validateCode(e.getCode(), code_duplicate_check_flag);
+        if(!code_error.equals("")){
+            errors.add(code_error);
+        }
+
+        String name_error = _validateName(e.getName());
+        if(!name_error.equals("")){
+            errors.add(name_error);
+        }
+
+        String password_error = _validatePassword(e.getPassword(),password_check_flag);
+        if(!password_error.equals("")){
+            errors.add(password_error);
+        }
+
+        return errors;
+    }
+
+    //社員番号
+    private static String _validateCode(String code, Boolean code_duplicate_check_flag){
+        //必須入力チェック
+        if(code == null || code.equals("")){
+            return "社員番号を入力してください";
+        }
+        //既に入力されている社員番号との重複チェック
+        if(code_duplicate_check_flag){
+            EntityManager em = DBUtil.createEntityManager();
+            long employees_count = (long)em.createNamedQuery("checkRegisteredCode", Long.class)
+                                          .setParameter("code", code)
+                                          .getSingleResult();
+        em.close();
+        if(employees_count > 0){
+            return "入力された社員番号の情報は既に存在しています。";
+        }
+        }
+
+        return "";
+    }
+
+    //社員名の必須入力チェック
+    private static String _validateName(String name){
+        if(name == null || name.equals("")){
+            return "氏名を入力してください。";
+        }
+
+        return "";
+    }
+
+    //パスワードの必須入力チェック
+    private static String _validatePassword(String password, Boolean password_check_flag){
+        //パスワードを変更する場合のみ実行
+        if(password_check_flag && (password == null || password.equals(""))){
+            return "パスワードを入力してください。";
+        }
+
+        return "";
+    }
+}
