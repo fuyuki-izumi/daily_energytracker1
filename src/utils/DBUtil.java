cf7052e161f6d808f7a3f1e4f3fcf8c8c98f5067
fuyuki.izumi <mac1@mac1-no-MacBook-Pro.local>
2020-04-19 18:27:35 +0900

    Add password encryyption

--- src/utils/DBUtil.java
+++ src/utils/DBUtil.java
@@ -3,7 +3,7 @@ package utils;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
+//様々なコントローラで使えるユーティリティ
 public class DBUtil {
     private static final String PERSISTENCE_UNIT_NAME ="daily_report_system";
     private static EntityManagerFactory emf;
---
+++ src/utils/EncryptUtil.java
@@ -0,0 +1,24 @@
+package utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.xml.bind.DatatypeConverter;
+
+public class EncryptUtil {
+    public static String getPasswordEncrypt(String plain_p, String salt) {
+        String ret = "";
+
+        if(plain_p != null && !plain_p.equals("")) {
+            byte[] bytes;
+            String password = plain_p + salt;
+            try {
+                bytes = MessageDigest.getInstance("SHA-256").digest(password.getBytes());
+                ret = DatatypeConverter.printHexBinary(bytes);
+            } catch(NoSuchAlgorithmException ex) {}
+        }
+
+        return ret;
+    }
+}
+
