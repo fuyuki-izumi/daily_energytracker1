11f70b98cd68840a12bf96c29381e428625e6906
fuyuki.izumi <mac1@mac1-no-MacBook-Pro.local>
2020-04-22 22:18:22 +0900

    Add login/logout

---
+++ src/controllers/login/LoginServlet.java
@@ -0,0 +1,101 @@
+package controllers.login;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import utils.DBUtil;
+import utils.EncryptUtil;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    //ログイン画面の表示
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("_token", request.getSession().getId());
+        request.setAttribute("hasError", false);
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/login/login.jsp");
+        rd.forward(request, response);
+    }
+
+    // ログイン処理を実行
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // 認証結果を格納する変数
+        Boolean check_result = false;
+
+        String code = request.getParameter("code");
+        String plain_pass = request.getParameter("password");
+
+        Employee e = null;
+
+        if(code != null && !code.equals("") && plain_pass != null && !plain_pass.equals("")) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            String password = EncryptUtil.getPasswordEncrypt(
+                    plain_pass,
+                    (String)this.getServletContext().getAttribute("salt")
+                    );
+
+            // 社員番号とパスワードが正しいかチェックする
+            try {
+                e = em.createNamedQuery("checkLoginCodeAndPassword", Employee.class)
+                      .setParameter("code", code)
+                      .setParameter("pass", password)
+                      .getSingleResult();
+            } catch(NoResultException ex) {}
+
+            em.close();
+
+            if(e != null) {
+                check_result = true;
+            }
+        }
+
+        if(!check_result) {
+            // 認証できなかったらログイン画面に戻る
+            request.setAttribute("_token", request.getSession().getId());
+            request.setAttribute("hasError", true);
+            request.setAttribute("code", code);
+
+            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/login/login.jsp");
+            rd.forward(request, response);
+        } else {
+            // 認証できたらログイン状態にしてトップページへリダイレクト
+            request.getSession().setAttribute("login_employee", e);
+
+            request.getSession().setAttribute("flush", "ログインしました。");
+            response.sendRedirect(request.getContextPath() + "/");
+        }
+    }
+
+}
+
---
+++ src/controllers/login/LogoutServlet.java
@@ -0,0 +1,37 @@
+package controllers.login;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LogoutServlet
+ */
+@WebServlet("/logout")
+public class LogoutServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LogoutServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //「セッションスコープに login_employeeという名前で従業員情報のオブジェクトを持っていること」をログインしている状態
+        request.getSession().removeAttribute("login_employee");
+
+        request.getSession().setAttribute("flush", "ログアウトしました。");
+        response.sendRedirect(request.getContextPath() + "/login");
+    }
+
+}
