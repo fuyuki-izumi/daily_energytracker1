689948e489f5f2c5814fa91e166bc1fbe77ffc31
fuyuki.izumi <mac1@mac1-no-MacBook-Pro.local>
2020-04-25 08:42:04 +0900

    Add Report show

---
+++ src/controllers/reports/ReportsShowServlet.java
@@ -0,0 +1,50 @@
+package controllers.reports;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Report;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class ReportsShowServlet
+ */
+@WebServlet("/reports/show")
+public class ReportsShowServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ReportsShowServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+        Report r = em.find(Report.class, Integer.parseInt(request.getParameter("id")));
+
+        em.close();
+
+        //日報とセッションIDをリクエストスコープにセットし
+        request.setAttribute("report", r);
+        request.setAttribute("_token", request.getSession().getId());
+
+        //show.jspを呼び出す
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/show.jsp");
+        rd.forward(request, response);
+    }
+
+}
